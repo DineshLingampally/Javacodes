564bbc651c7ffa02219bbb58a008f8c1ef027a4d
dlingampally <dineshlingampally25848@gmail.com>
2024-12-04 13:59:32 +0530

    commit 2

--- App.java
+++ App.java
@@ -1,5 +1,8 @@
 class App{
     public static void main(String[] args){
-        System.out.println("Hello Dinesh");
+
+        System.out.println("Hello Dinesh Rao");
+        System.out.println("HEllo infor");
+        System.out.println("Hello World");
     }
 }
